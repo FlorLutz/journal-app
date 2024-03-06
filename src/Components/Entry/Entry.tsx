90097d7b15ca5ab99cbdf93a9d1eb7d4e6488d5d
Florian Lutz <florian.lutz@ymail.com>
2024-03-06 18:05:57 +0100

    final changes

--- src/Components/Entry/Entry.tsx
+++ src/Components/Entry/Entry.tsx
@@ -2,8 +2,9 @@ import styles from "./Entry.module.css";
 import star from "./../../assets/star.svg";
 import starFilled from "./../../assets/star-filled.svg";
 
-export default function Entries({ date, headline, content, marked }) {
-  return (
+type props = {date: string, headline: string, content: string, marked: boolean}
+
+export default function Entries({ date, headline, content, marked }: props){
     <>
       <p className={styles.entry__date}>{date}</p>
       <div className={styles.entry__headlineContainer}>
